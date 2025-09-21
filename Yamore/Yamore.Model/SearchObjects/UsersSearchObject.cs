a54887a4e996858caac593a492c8682b1b69f1e7
Zerina Ibrulj <zerina.ibrulj@edu.fit.ba>
2025-09-21 18:11:22 +0200

    Adding UsersSearchObject

---
+++ Yamore/Yamore.Model/SearchObjects/UsersSearchObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yamore.Model.SearchObjects
+{
+    public class UsersSearchObject
+    {
+        public string? FirstNameGTE { get; set; }
+        public string? LastNameGTE { get; set; }
+        public string? Email { get; set; }
+        public string? Username { get; set; }
+    }
+}
