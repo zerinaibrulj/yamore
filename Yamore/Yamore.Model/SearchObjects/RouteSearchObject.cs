d6024172183aac8802e9fd602cc282a27d3f1283
Zerina Ibrulj <zerina.ibrulj@edu.fit.ba>
2025-10-03 13:19:52 +0200

    Comment for RouteSearchObject

--- Yamore/Yamore.Model/SearchObjects/RouteSearchObject.cs
+++ Yamore/Yamore.Model/SearchObjects/RouteSearchObject.cs
@@ -6,9 +6,9 @@ namespace Yamore.Model.SearchObjects
 {
     public class RouteSearchObject : BaseSearchObject
     {
-        public int StartCityId { get; set; }
+        public int? StartCityId { get; set; }       // stavit cemo ? na sve propertije ako mislimo da nam Get metoda radi i bez da posaljemo parametre po kojima cemo pretrazivati, na ovaj nacin ce nam vratiti sve zapise iz baze
 
-        public int EndCityId { get; set; }
+        public int? EndCityId { get; set; }
 
         public int? EstimatedDurationHours { get; set; }
     }
